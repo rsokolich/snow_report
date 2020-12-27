ed52f9a2463ccb18a5fc5970c7d78542c413fa28
rsokolich <53921009+rsokolich@users.noreply.github.com>
2020-12-27 14:18:02 -0500

    base app

---
+++ config_sample.py
@@ -0,0 +1,8 @@
+# .gitignore should include a reference to config.py
+
+# Enter OpenWeather's API key:
+ow_api_key = 'EnterOpenWeathersAPIKey'
+
+# Enter IFTTT alert name and API key:
+ifttt_alert_name = "EnterNameofIFTTAlert"
+ifttt_alert_key = "EnterIFTTAPIKey"
